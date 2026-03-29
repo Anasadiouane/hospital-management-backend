91545bf28482e212313d7923ca77bd71fa79cd0f
anasadiouane <anasadiouane5@gmail.com>
2026-03-29 13:37:20 +0100

    add Employee service

---
+++ src/main/java/com/hospital/hospital_management_backend/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.hospital.hospital_management_backend.service;
+
+import com.hospital.hospital_management_backend.dto.request.EmployeeDtoRequest;
+import com.hospital.hospital_management_backend.dto.request.ServiceDtoRequest;
+import com.hospital.hospital_management_backend.dto.response.EmployeeDtoResponse;
+import com.hospital.hospital_management_backend.dto.response.FileDtoResponse;
+import com.hospital.hospital_management_backend.dto.response.PageDtoResponse;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDtoResponse createEmployee(EmployeeDtoRequest employeeDto);
+    EmployeeDtoResponse getEmployeeById(Long employeeId);
+    EmployeeDtoResponse getEmployeeByCin(String cin);
+    EmployeeDtoResponse getEmployeeByRN(String registrationNumber);
+    List<EmployeeDtoResponse> getAllEmployees();
+    PageDtoResponse getAllEmployees(Integer pageNumber, Integer pageSize, String sortField, String sortDirection);
+    List<FileDtoResponse> getDocumentsOfEmployee(Long employeeId);
+    PageDtoResponse getEmployeesByService(Long serviceId, Integer pageNumber, Integer pageSize, String sortField, String sortDirection);
+    EmployeeDtoResponse updateEmployee(EmployeeDtoRequest employeeDto, Long employeeId);
+    EmployeeDtoResponse addDocumentToEmployee(Long employeeId, Long documentId);
+    EmployeeDtoResponse addServiceToEmployee(Long employeeId, ServiceDtoRequest serviceDtoRequest);
+    void deleteEmployee(Long employeeId);
+    void deleteDocumentOfEmployee(Long employeeId, Long documentId);
+}
